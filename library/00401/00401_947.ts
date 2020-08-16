083e7c06d627142ce561f4b211c5563a243d3d63
sam <sam@freighttrust.com>
2020-08-16 04:14:39 -0700

    feat: 004010 X12 Set

--- library/00401/00401_947.ts
+++ library/00401/00401_947.ts
@@ -1,3 +1,9 @@
+/*SPDX-License-Identifier: MIT
+SPDXVersion: SPDX-2.2
+SPDX-FileCopyrightText: Copyright 2020 Sam Bacha
+This Source Code Form is subject to the terms of the MIT License
+If a copy of the MPL was not distributed with this
+file, You can obtain one at hhttps://spdx.org/licenses/MIT.html */
 "use strict";
 var validate = (function () {
   var refVal = [];
